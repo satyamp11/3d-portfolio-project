0856f85723915024c43b8c2cae8b300fb3ba2991
satyamp11 <imsattu8601@gmail.com>
2026-03-17 20:31:17 +0530

    final api fix for deployment

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setStatus("");
 
     try {
-      const res = await fetch("http://localhost:5000/api/contact", {
+      const res = await fetch("https://three-d-portfolio-project.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
