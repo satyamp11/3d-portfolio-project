3846a002994df1b199cedc33feeadd53fb457080
satyamp11 <imsattu8601@gmail.com>
2026-03-18 11:38:12 +0530

    fix render url

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
     setStatus("");
 
     try {
-      const res = await fetch("https://three-d-portfolio-project.onrender.com/api/contact", {
+      const res = await fetch("https://threed-portfolio-project.onrender.com/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
